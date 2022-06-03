3d270891adbb8b81634e6496c773c9682de8b57e
chenhantao <372101345@qq.com>
2022-06-04 02:00:43 +0800

    add

---
+++ src/cht/leetcode/java/code21/Solution.java
@@ -0,0 +1,64 @@
+package cht.leetcode.java.code21;
+
+import cht.leetcode.java.ListNode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ *
+ *
+ * 示例 1：
+ * 输入：l1 = [1,2,4], l2 = [1,3,4]
+ * 输出：[1,1,2,3,4,4]
+ * 示例 2：
+ *
+ * 输入：l1 = [], l2 = []
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：l1 = [], l2 = [0]
+ * 输出：[0]
+ *  
+ *
+ * 提示：
+ *
+ * 两个链表的节点数目范围是 [0, 50]
+ * -100 <= Node.val <= 100
+ * l1 和 l2 均按 非递减顺序 排列
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/merge-two-sorted-lists">https://leetcode.cn/problems/merge-two-sorted-lists</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author chenhantao
+ * @since 2022/5/16
+ */
+public class Solution {
+
+    /**
+     * 递归实现
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        // list1的val小于list2的val, 那list1.next就指向list1的下一个node和当前list2
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+
+    }
+
+}
