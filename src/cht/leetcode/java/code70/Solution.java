ff7045d7000acd9a0ff70a2bbc013abc5cb31ca3
chenhantao <372101345@qq.com>
2022-06-09 03:16:05 +0800

    add

---
+++ src/cht/leetcode/java/code70/Solution.java
@@ -0,0 +1,53 @@
+package cht.leetcode.java.code70;
+
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 解释：有两种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶
+ * 2. 2 阶
+ * 示例 2：
+ *
+ * 输入：n = 3
+ * 输出：3
+ * 解释：有三种方法可以爬到楼顶。
+ * 1. 1 阶 + 1 阶 + 1 阶
+ * 2. 1 阶 + 2 阶
+ * 3. 2 阶 + 1 阶
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 45
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：<a href="https://leetcode.cn/problems/climbing-stairs">https://leetcode.cn/problems/climbing-stairs</a>
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author chenhantao
+ * @since 2022/5/17
+ */
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        int[] result = new int[n];
+
+        result[0] = 1;
+        result[1] = 2;
+        for (int i = 2; i < n; i++) {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+        return result[n - 1];
+    }
+}
